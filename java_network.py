54c7d617b86279e689ef3ce4ad44eb2c2ab78dee
oliver-helix <oliver.tucher@helix.com>
2022-01-21 17:36:07 -0800

    structure organization

--- java_network.py
+++ java_network.py
@@ -1,4 +1,31 @@
+from cgi import test
 import subprocess
+import sys
+
+$bitvector
+$FILE
+$P_THR
+$S_THR
+$D_THR
 
 
-subprocess.run()
+subprocess.run(
+    java -cp stepminer-1.1.jar -Xms64m -Xmx10G tools.CustomAnalysis \
+        boolean bitMatrix $FILE.rl \
+        $bitvector \
+        false_filler.ph All $P_THR $S_THR $D_THR
+)
+
+subprocess.run(
+    java -cp stepminer-1.1.jar -Xms64m -Xmx10G tools.CustomAnalysis \
+    boolean bitMatrixFill $FILE.rl
+)
+
+subprocess.run(
+    java -cp stepminer-1.1.jar -Xms64m -Xmx10G tools.CustomAnalysis \
+    boolean bitMatrixFillStats $FILE.rl
+)
+
+GSM PROBEID    000111002220000
+
+/booleanfs2/sahoo/Data/Brain/RNASeq
