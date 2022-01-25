8f551f686d511d1c87d1b5fa460663d6df24e4f6
oliver-helix <oliver.tucher@helix.com>
2022-01-25 12:14:40 -0800

    added docstrings, seperated methods and added __main__ call

--- network.py
+++ network.py
@@ -5,6 +5,9 @@ import pandas as pd
 
 from dataclasses import dataclass
 
+from .bone import BoNE
+from .geo import GEO
+
 # java version: jdk1.8.0_45
 
 
@@ -23,24 +26,36 @@ class Stepminer:
             )
             bv_file = "bitvector.txt"
             self.bv["BitVector"].to_csv(bv_file, sep="\t")
-        elif isinstance(self.bv, str):
-            bv_file = self.bv
+            self.bv = bv_file
 
         file_path = os.path.dirname(os.path.abspath(__file__))
-        stepminer = os.path.join(file_path, "references/stepminer-1.1.jar")
+        self.stepminer_path = os.path.join(file_path, "references/stepminer-1.1.jar")
 
+        self.step1()
+        self.step2()
+        self.step3()
+        self.human_readable()
+
+    def setp1(self):
+        """
+        export CLASSPATH="./stepminer-1.1.jar"
+        stepminer="java -cp $CLASSPATH -Xms64m -Xmx10G tools.CustomAnalysis"
+        ${stepminer} boolean bitMatrix $FILE.rl \
+            data/peters-2017-ibd-bv.txt \
+            $FILE.ph All 0.1 2.5 0.05
+        """
         subprocess.run(
             [
                 "java",
                 "-cp",
-                stepminer,
+                self.stepminer_path,
                 "-Xms64m",
                 "-Xmx10G",
                 "tools.CustomAnalysis",
                 "boolean",
                 "bitMatrix",
                 self.file_rl,
-                bv_file,
+                self.bv,
                 "false_filler.ph",
                 "All",
                 str(self.p_thr),
@@ -48,11 +63,19 @@ class Stepminer:
                 str(self.d_thr),
             ]
         )
+
+    def step2(self):
+        """
+        export CLASSPATH="./stepminer-1.1.jar"
+        stepminer="java -cp $CLASSPATH -Xms64m -Xmx10G tools.CustomAnalysis"
+        cp $FILE.rl $FILE-1.rl
+        ${stepminer} boolean bitMatrixFill $FILE-1.rl
+        """
         subprocess.run(
             [
                 "java",
                 "-cp",
-                stepminer,
+                self.stepminer_path,
                 "-Xms64m",
                 "-Xmx10G",
                 "tools.CustomAnalysis",
@@ -61,11 +84,18 @@ class Stepminer:
                 self.file_rl,
             ]
         )
+
+    def step3(self):
+        """
+        export CLASSPATH="./stepminer-1.1.jar"
+        stepminer="java -cp $CLASSPATH -Xms64m -Xmx10G tools.CustomAnalysis"
+        ${stepminer} boolean bitMatrixFillStats $FILE-1.rl
+        """
         subprocess.run(
             [
                 "java",
                 "-cp",
-                stepminer,
+                self.stepminer_path,
                 "-Xms64m",
                 "-Xmx10G",
                 "tools.CustomAnalysis",
@@ -74,13 +104,20 @@ class Stepminer:
                 self.file_rl,
             ]
         )
+
+    def human_readable(self):
+        """
+        export CLASSPATH="./stepminer-1.1.jar"
+        stepminer="java -cp $CLASSPATH -Xms64m -Xmx10G tools.CustomAnalysis"
+        ${stepminer} boolean bitMatrixPrint $FILE-1.rl > $FILE-res.txt
+        """
         # create -res.txt for human readable results
         file_res_txt = self.file_rl.split(".")[0] + "-res.txt"
         subprocess.run(
             [
                 "java",
                 "-cp",
-                stepminer,
+                self.stepminer_path,
                 "-Xms64m",
                 "-Xmx10G",
                 "tools.CustomAnalysis",
@@ -91,3 +128,13 @@ class Stepminer:
                 file_res_txt,
             ]
         )
+
+
+if __name__ == "__main__":
+    gse40060 = GEO(accessionID="GSE40060")
+    survival = gse40060.survival()
+    expr = gse40060.expr(rename_genes=True, probeID="ENSG")
+    expr = expr.fillna(0)
+    dong = BoNE(expr, survival)
+    bv = dong.bv()
+    Stepminer(bv)
